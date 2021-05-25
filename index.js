faf2351bc980dfab486e1bc1ed91417e29ced93d
Your Username <you@example.com>
2021-05-25 12:41:15 -0400

    commit tree 5ab1725477ddb206b080009899ae0663c1ea20b3

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+] 
 
+function superbowlWin(record) { 
+    // record.find(record[year] => result === "W")
+    const winningRecord = record.find((object) => object.result === "W")  
+    
+    if (winningRecord === undefined) {  
+        return undefined 
+    } else { 
+        return winningRecord.year
+    }
+} 
